a16f4df79a765c166e1cbe749e3b4f78c7f3ca8b
Changsik Jang <lowmans00@gmail.com>
2021-08-09 18:23:48 +0900

    refactor: clean code

--- src/views/Konva.vue
+++ src/views/Konva.vue
@@ -213,36 +213,22 @@ export default {
       )
 
       textNode.on("transform", function() {
-        // reset scale, so only with is changing by transformer
         textNode.setAttrs({
           width: textNode.width() * textNode.scaleX(),
           scaleX: 1
         })
       })
       textNode.on("dblclick dbltap", () => {
-        // hide text node and transformer:
         textNode.hide()
-
-        // create textarea over canvas with absolute position
-        // first we need to find position for textarea
-        // how to find it?
-
-        // at first lets find position of text node relative to the stage:
         const textPosition = textNode.absolutePosition()
-
-        // so position of textarea will be the sum of positions above:
         const areaPosition = {
           x: this.stage.container().offsetLeft + textPosition.x,
           y: this.stage.container().offsetTop + textPosition.y
         }
 
-        // create textarea and style it
         const textarea = document.createElement("textarea")
         document.body.appendChild(textarea)
 
-        // apply many styles to match text on canvas as close as possible
-        // remember that text rendering on canvas and on the textarea can be different
-        // and sometimes it is hard to make it 100% the same. But we will try...
         textarea.value = textNode.text()
         textarea.style.position = "absolute"
         textarea.style.top = areaPosition.y + "px"
@@ -262,6 +248,7 @@ export default {
         textarea.style.transformOrigin = "left top"
         textarea.style.textAlign = textNode.align()
         textarea.style.color = textNode.fill()
+
         const rotation = textNode.rotation()
         let transform = ""
         if (rotation) {
@@ -269,21 +256,14 @@ export default {
         }
 
         let px = 0
-        // also we need to slightly move textarea on firefox
-        // because it jumps a bit
         const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1
         if (isFirefox) {
           px += 2 + Math.round(textNode.fontSize() / 20)
         }
         transform += "translateY(-" + px + "px)"
-
         textarea.style.transform = transform
-
-        // reset height
         textarea.style.height = "auto"
-        // after browsers resized it we can set actual value
         textarea.style.height = textarea.scrollHeight + 3 + "px"
-
         textarea.focus()
 
         function removeTextarea() {
@@ -294,16 +274,13 @@ export default {
 
         function setTextareaWidth(newWidth) {
           if (!newWidth) {
-            // set width for placeholder
             newWidth = textNode.placeholder.length * textNode.fontSize()
           }
-          // some extra fixes on different browsers
           const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
           const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1
           if (isSafari || isFirefox) {
             newWidth = Math.ceil(newWidth)
           }
-
           const isEdge = document.documentMode || /Edge/.test(navigator.userAgent)
           if (isEdge) {
             newWidth += 1
@@ -311,20 +288,17 @@ export default {
           textarea.style.width = newWidth + "px"
         }
 
-        textarea.addEventListener("keydown", function(e) {
-          // hide on enter
-          // but don't hide on shift + enter
+        textarea.addEventListener("keydown", e => {
           if (e.keyCode === 13 && !e.shiftKey) {
             textNode.text(textarea.value)
             removeTextarea()
           }
-          // on esc do not set value back to node
           if (e.keyCode === 27) {
             removeTextarea()
           }
         })
 
-        textarea.addEventListener("keydown", function() {
+        textarea.addEventListener("keydown", () => {
           const scale = textNode.getAbsoluteScale().x
           setTextareaWidth(textNode.width() * scale)
           textarea.style.height = "auto"
